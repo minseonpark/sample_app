3371719d7739ec5b0e23f567932f9cd157c16758
minseonpark <msnsunms@naver.com>
2011-01-07 11:06:02 +0900

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
   get "pages/home"
 
   get "pages/contact"
+  get "Pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
